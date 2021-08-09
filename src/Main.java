d7fe61891620526c5bfe197558af91fc85798614
Davide Corsi <davide.corsi@par-tec.it>
2021-08-09 14:57:07 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/*
+	Scrivere un applicazione che data una stringa in ingresso riconosca se sia palindroma o meno
+*/
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Inserire la parola da controllare ");
+		String s = input.nextLine();
+		// Per estrarre un carattere dalla stringa potete usare il metodo charAt es: s = "pippo" s.charAt(0) -> 'p'
+	}
+}
